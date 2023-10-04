a2fd9117fd664a81081d0605d64a0fb2a41324b5
BKidir <126101875+BKidir@users.noreply.github.com>
2023-10-04 15:29:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+const menu = document.querySelector(".hamburger-menu");
+const closeIcon = document.querySelector(".close-icon");
+const nav = document.querySelector("nav");
+
+menu.addEventListener('click', () => {
+    nav.classList.add('active');
+    closeIcon.classList.add('show');
+});
+
+closeIcon.addEventListener('click', () => {
+    nav.classList.remove('active');
+    closeIcon.classList.remove('show');
+});
+
+document.addEventListener("click", function (e) {
+  
+    if (!nav.contains(e.target) && !closeIcon.contains(e.target) && !menu.contains(e.target)){
+      
+        nav.classList.remove('active');
+        closeIcon.classList.remove('show');
+      
+    }
+});
+
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const href = link.getAttribute("href");
+    window.location.href = href;
+
+    // Remove active class from all links
+    navLinks.forEach((navLink) => {
+      navLink.classList.remove("active-link");
+    });
+
+    // Add active class to clicked link
+    link.classList.add("active-link");
+  });
+});
+
+const destinationLinks = document.querySelectorAll(".destinations a");
+
+destinationLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+        const href = link.getAttribute("href");
+        window.location.href = href;
+
+        destinationLinks.forEach((destinationLink) => {
+            destinationLink.classList.remove("active-destination");
+        });
+
+        link.classList.add("active-destination")
+    });
+
+});
