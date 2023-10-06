8ca63f34441058de10569e45f0b8af462d7449a8
BKidir <126101875+BKidir@users.noreply.github.com>
2023-10-06 14:19:12 +0100

    owl carousel implemented

--- script.js
+++ script.js
@@ -2,24 +2,25 @@ const menu = document.querySelector(".hamburger-menu");
 const closeIcon = document.querySelector(".close-icon");
 const nav = document.querySelector("nav");
 
-menu.addEventListener('click', () => {
-    nav.classList.add('active');
-    closeIcon.classList.add('show');
+menu.addEventListener("click", () => {
+  nav.classList.add("active");
+  closeIcon.classList.add("show");
 });
 
-closeIcon.addEventListener('click', () => {
-    nav.classList.remove('active');
-    closeIcon.classList.remove('show');
+closeIcon.addEventListener("click", () => {
+  nav.classList.remove("active");
+  closeIcon.classList.remove("show");
 });
 
 document.addEventListener("click", function (e) {
-  
-    if (!nav.contains(e.target) && !closeIcon.contains(e.target) && !menu.contains(e.target)){
-      
-        nav.classList.remove('active');
-        closeIcon.classList.remove('show');
-      
-    }
+  if (
+    !nav.contains(e.target) &&
+    !closeIcon.contains(e.target) &&
+    !menu.contains(e.target)
+  ) {
+    nav.classList.remove("active");
+    closeIcon.classList.remove("show");
+  }
 });
 
 const navLinks = document.querySelectorAll("nav a");
@@ -43,16 +44,15 @@ navLinks.forEach((link) => {
 const destinationLinks = document.querySelectorAll(".destinations a");
 
 destinationLinks.forEach((link) => {
-    link.addEventListener("click", (event) => {
-        event.preventDefault();
-        const href = link.getAttribute("href");
-        window.location.href = href;
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const href = link.getAttribute("href");
+    window.location.href = href;
 
-        destinationLinks.forEach((destinationLink) => {
-            destinationLink.classList.remove("active-destination");
-        });
-
-        link.classList.add("active-destination")
+    destinationLinks.forEach((destinationLink) => {
+      destinationLink.classList.remove("active-destination");
     });
 
+    link.classList.add("active-destination");
+  });
 });
